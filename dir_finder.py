37465ddf86e68da439494b9429425fb55d85a1fd
Kirito340866 <zni35826@gmail.com>
2026-06-24 13:41:57 +0630

    Update version to 1.1 and improve request handling

--- dir_finder.py
+++ dir_finder.py
@@ -19,7 +19,7 @@ print("""
               ▓▓▓▓▓▓█████████▓▓▓▓▓▓▓▓▓▓██████████▓▓▓▒▓▓              
                    ▓▒▒▓▓▓████████████████████▓▓▓▓                    
                           ▓▓▓▓████████████                                                                                     
-v1.0      
+v1.1
 """)
 print("\n========[Dir Finder]========\n")
 print("[*] Version: 1.0\n")
@@ -34,52 +34,52 @@ word_list = flited.wordlist
 target = flited.url
 fake_response_length = None
 def fake_response_finder():
-        global fake_response_length
-        try:
-                fake_url = f"{target}/alkdfiaoijeoijlkfja;lkdfoiejfjajdoj--=3=+d==dlkd?kdlfka?jdkfa=ld"
-                fake_result = requests.get(fake_url)
-                fake_response_length = len(fake_result.text)
-                print(f"[/] Fake Response is Detected:           Size: {len(fake_result.text)} Bytes\n")
-        except Exception as e:
-                return None
+	global fake_response_length
+	try:
+		fake_url = f"{target}/alkdfiaoijeoijlkfja;lkdfoiejfjajdoj--=3=+d==dlkd?kdlfka?jdkfa=ld"
+		fake_result = requests.get(fake_url, verify=False, timeout=3)
+		fake_response_length = len(fake_result.text)
+		print(f"[/] Fake Response is Detected:           Size: {len(fake_result.text)} Bytes\n")
+	except Exception as e:
+		return None
 def finder(doors):
-        global fake_response_length
-        try:
-                clean_door = doors.strip()
-                combine_url = f"{target}/{clean_door}"
-                result = requests.get(combine_url,timeout=5)
-                if result.status_code in [200,300,301,500]:
-                        if len(result.text) == fake_response_length:
-                                pass
-                        else:
-                                print(f"[+] {combine_url}                       Status Code: {result.status_code}")
-                else:
-                        pass
-        except Exception as e:
-                return None
+	global fake_response_length
+	try:
+		clean_door = doors.strip()
+		combine_url = f"{target}/{clean_door}"
+		result = requests.get(combine_url,timeout=5, verify=False)
+		if result.status_code in [200,300,301,500]:
+			if len(result.text) == fake_response_length:
+				pass
+			else:
+				print(f"[+] {combine_url}			Status Code: {result.status_code}")
+		else:
+			pass
+	except Exception as e:
+		return None
 def main():
-        print(f"[*] Target:     {flited.url}")
-        print(f"[*] Wordlist:   {flited.wordlist}")
-        print(f"[*] Thread:     {flited.thread}\n")
-        print(f"[/] Start Finding...\n\n")
-        fake_response_finder()
-        try:
-                with open(word_list,'r') as d_file:
-                        file_end = False
-                        while not file_end:
-                                door_list = []
-                                for u in range(500):
-                                        line = d_file.readline()
-                                        if not line:
-                                                file_end = True
-                                                break
-                                        if line.strip():
-                                                door_list.append(line.strip())
-                                with ThreadPoolExecutor(max_workers=int(flited.thread)) as executor:
-                                        executor.map(finder,door_list)
-        except KeyboardInterrupt:
-                print("[-] Exiting...")
-                exit()
-        except Exception as e:
-                print(f"[-] Error: {e}")
+	print(f"[*] Target:	{flited.url}")
+	print(f"[*] Wordlist: 	{flited.wordlist}")
+	print(f"[*] Thread:	{flited.thread}\n")
+	print(f"[/] Start Finding...\n\n")
+	fake_response_finder()
+	try:
+		with open(word_list,'r') as d_file:
+			file_end = False
+			while not file_end:
+				door_list = []
+				for u in range(500):
+					line = d_file.readline()
+					if not line:
+						file_end = True
+						break
+					if line.strip():
+						door_list.append(line.strip())
+				with ThreadPoolExecutor(max_workers=int(flited.thread)) as executor:
+					executor.map(finder,door_list)
+	except KeyboardInterrupt:
+		print("[-] Exiting...")
+		exit()
+	except Exception as e:
+		print(f"[-] Error: {e}")
 main()
